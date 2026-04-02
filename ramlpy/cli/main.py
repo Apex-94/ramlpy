66e61737cc2625ae39af62ba099cfba635ea742b
Apex-94 <Apex-94@users.noreply.github.com>
2026-04-03 01:35:28 +0530

    Add info command to CLI

--- ramlpy/cli/main.py
+++ ramlpy/cli/main.py
@@ -25,12 +25,19 @@ def main():
     validate_parser = subparsers.add_parser('validate', help='Validate a RAML file')
     validate_parser.add_argument('file', help='RAML file to validate')
     
+    # Info command
+    info_parser = subparsers.add_parser('info', help='Show API information')
+    info_parser.add_argument('file', help='RAML file to inspect')
+    info_parser.add_argument('--json', action='store_true', help='Output as JSON')
+    
     args = parser.parse_args()
     
     if args.command == 'parse':
         cmd_parse(args)
     elif args.command == 'validate':
         cmd_validate(args)
+    elif args.command == 'info':
+        cmd_info(args)
     else:
         parser.print_help()
         sys.exit(1)
@@ -72,6 +79,37 @@ def cmd_validate(args):
         print("Resources: %d" % len(api.resources))
     except RamlError as e:
         print("Validation failed: %s" % e, file=sys.stderr)
+        sys.exit(1)
+
+
+def cmd_info(args):
+    """Handle info command."""
+    try:
+        api = parse(args.file)
+        if getattr(args, 'json', False):
+            output = {
+                'title': api.title,
+                'version': api.version,
+                'base_uri': api.base_uri,
+                'media_type': api.media_type,
+                'resources': [r.full_path for r in api.resources],
+                'types': list(api.types.keys()),
+            }
+            print(json.dumps(output, indent=2))
+        else:
+            print("Title: %s" % api.title)
+            print("Version: %s" % api.version)
+            print("Base URI: %s" % api.base_uri)
+            print("Media Type: %s" % api.media_type)
+            print("Resources: %d" % len(api.resources))
+            for resource in api.resources:
+                print("  %s" % resource.full_path)
+                for method_name in resource.methods:
+                    print("    - %s" % method_name.upper())
+            if api.types:
+                print("Types: %s" % ', '.join(api.types.keys()))
+    except RamlError as e:
+        print("Error: %s" % e, file=sys.stderr)
         sys.exit(1)
 
 
